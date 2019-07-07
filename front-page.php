4c1609e51c06dfb34bafc78f78bfaa450aeb7f6d
GaryLamKa <39815026+GaryLamKa@users.noreply.github.com>
2019-07-08 08:35:57 +1000

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,5 @@
+<?php get_header();?>
+
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+<?php get_footer();?>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+        <head>
+
+            <?php wp_head();?>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        </head>
+
+<body <?php body_class();?>>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  <header>
+    <div class="container">
+        <nav>
+            <h1 class="brand"><a href="http://localhost/wordpress/">Instisap</a></h1>
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Shop</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Links</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Events</a></li>
+                <li><a href="">Cart</a></li>
+            </ul>
+        </nav>
+    </div>
+
+
+
+  </header>
